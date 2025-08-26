e89a576b80bb59ea3977a5faeac02a1419e3265f
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2025-08-26 18:22:27 -0400

    initial commit: project setup

---
+++ src/router.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "experience", element: <Experience /> },
+      { path: "projects", element: <Projects /> },
+      { path: "resume", element: <Resume /> },
+      { path: "contact", element: <Contact /> },
+      { path: "*", element: <NotFound /> }
+    ]
+  }
+]);
